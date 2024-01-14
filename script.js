43b37a3d76bd716865e9ad8083635e639f8e2cf0
Shroud <pseudo_guy890@proton.me>
2024-01-14 16:30:22 +0530

    computerChoice() and playRound() has been set

---
+++ script.js
@@ -0,0 +1,51 @@
+function getComputerChoice() {
+  let randomNum = Math.floor((Math.random() * 3) + 1);
+  let choice;
+  if (randomNum === 1) {
+     choice = "rock";
+  }
+  else if (randomNum === 2) {
+     choice = "paper";
+  
+}
+else if (randomNum === 3) {
+  choice =  "scissor";
+}
+return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+      
+      playerSelection = playerSelection.toLowerCase();
+     
+      if (playerSelection === computerSelection) {
+         console.log("Looks like It's a tie");
+      }
+      else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+           console.log("You win, Rock beats Scissor");
+      }
+      else if(playerSelection === 'rock' && computerSelection === 'paper') {
+         console.log("You lose, paper beats Rock");
+      }
+      else if(playerSelection === 'paper' && computerSelection === 'rock') {
+         console.log("You win, paper beats rock");
+      }
+      else if(playerSelection === 'paper' && computerSelection === 'scissor'){
+        console.log("You lose, scissor beats paper");
+      }
+      else if(playerSelection === 'scissor' && computerSelection === 'rock'){
+        console.log("You lose, rock beats scissor");
+      
+      } 
+      else if(playerSelection === 'scissor' && computerSelection === 'paper'){
+        console.log("You win, scissor beats paper");
+      }
+      else {
+        console.log("Invalid input ")
+      }      
+       
+      
+      }
+playerSelection = "PAPER";
+
+playRound(playerSelection, getComputerChoice());
