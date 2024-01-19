b1f3dca1c9d84b2ce35220fba50fa485dfea5c66
Shroud <pseudo_guy890@proton.me>
2024-01-19 18:58:52 +0530

    score evalauator fixed

--- script.js
+++ script.js
@@ -18,92 +18,109 @@ function getPlayerChoice(){
   let playerInput = prompt("choose your weapon");
    caseNormalize = playerInput.toLowerCase();
    return caseNormalize;
-
 }
 
-function getScore(){
-  let PlayerCounter = 0;
+let playerCounter = 0;
   let compCounter = 0;
-  let score;
-  playerSelection = getPlayerChoice();
-  computerSelection = getComputerChoice();
-  score = playRound(playerSelection, computerSelection);
-    if(score === "you win, Rock beats scissor"|| score === "you win, paper beats rock" || score === "you win, scissor beats paper") {
-       return PlayerCounter++;
-    }
-    else if(score === "you lose, paper beats rock"|| score === "you lose, scissor beats paper" || score === "you lose, scissor beats rock"){
-      return compCounter ++;
-    }
-  
-   }
-  
+
+
 function playRound(playerSelection, computerSelection){
   
   
   if(playerSelection === computerSelection){
-    return "tie";
+    console.log("------------------");
+    return " Looks like its a tie";
 
   }
    else if(playerSelection === 'rock' && computerSelection === 'scissor'){
-     
-    return "you win, Rock beats scissor";
+     playerCounter++;
+     console.log("------------------")
+    return  `computer: ${computerSelection}  
+you win, rock beats scissor`;
    }
    else if(playerSelection === 'rock' && computerSelection === 'paper'){
-   
-    return "you lose, paper beats rock";
+      compCounter++;
+      console.log("------------------")
+    return `computer: ${computerSelection}    
+you lose, paper beats rock`;
    }
    else if(playerSelection === 'paper' && computerSelection === 'rock'){
-    
-    return "you win, paper beats rock";
+    playerCounter++;
+    console.log("------------------")
+    return `computer: ${computerSelection}   
+you win, paper beats rock`;
    }
    else if(playerSelection === 'paper' && computerSelection === 'scissor'){
-    
-    return "you lose, scissor beats paper";
+    compCounter++;
+    console.log("------------------")
+    return `computer: ${computerSelection}   
+you lose, scissor beats paper`;
    }
    else if(playerSelection === 'scissor' && computerSelection === 'paper'){
-    
-    return "you win, scissor beats paper";
+    playerCounter++;
+    console.log("------------------");
+    return `computer: ${computerSelection}   
+you win, scissor beats paper`;
    }
    else if(playerSelection === 'scissor' && computerSelection === 'rock' ){
-    
-    return "you lose, scissor beats rock"; 
+    compCounter++;
+    console.log("------------------")
+    return `computer: ${computerSelection}   
+you lose, rock beats scissor`; 
 
-   }
-   else {
-    return null;
    }
    
-
   }
-
+  
+ 
   function game(){
-    playerSelection = getPlayerChoice();
-  computerSelection = getComputerChoice();
+   
+    
+  //  playerSelection = getPlayerChoice();
+  // computerSelection = getComputerChoice();
+ console.log(playRound(getPlayerChoice(), getComputerChoice()));
+ console.log(playRound(getPlayerChoice(), getComputerChoice()));
+ console.log(playRound(getPlayerChoice(), getComputerChoice()));
+ console.log(playRound(getPlayerChoice(), getComputerChoice()));
+ console.log(playRound(getPlayerChoice(), getComputerChoice()));
+ console.log("------------------");
+ if(playerCounter > compCounter){
+  console.log("**you are the winner of the match**")
+ }
+ else if(compCounter > playerCounter){
+  console.log("**you lose, Computer Beats you**")
+ }
+ else {
+  console.log("**TIE**")
+ }
   
   
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  playRound(playerSelection, computerSelection);
-  getScore();
-  if(playerCounter > compCounter){
-    console.log("Congratulaions, you are the winner");
-  }
-  else if(playercounter < compCounter){
-    console.log("sorry, you lost the game");
-  }
-  else{
-    console.log("it's a tie ,match");
-  }
+   
+  
+}
+  game();
   
   
-
-  }
   
   
-  console.log(game());
   
   
-
+  
+  
+  
+ 
+  
+  
+  
+ 
+  
+  
+  
+  
+ 
+  
+   
+  
+ 
+  
   
